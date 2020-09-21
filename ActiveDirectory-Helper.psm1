070b6663b3722bc5e411204d1d5ab547ab0546ba
PsCustomObject <PsCustomObject@outlook.com>
2020-09-21 12:57:51 +0200

    Added function Convert-ADExpirationDate cmdlet
    Signed-off-by: PsCustomObject <PsCustomObject@outlook.com>

---
+++ ActiveDirectory-Helper.psm1
@@ -0,0 +1,52 @@
+﻿function Convert-ADExpirationDate
+{
+<#
+    .SYNOPSIS
+        Cmdlet is used to convert a user expiration date to human redeable format
+    
+    .DESCRIPTION
+        Cmdlet is used to convert internal AD expiration date format to human redeable format.
+    
+    .PARAMETER ExpirationDate
+        A string or long representing the expiration date as exposed by the accountExpires attribute in Active Directory.
+    
+    .EXAMPLE
+        PS C:\> Convert-AdExpirationDate -ExpirationDate 132566400000000000
+#>
+    
+    [CmdletBinding(ConfirmImpact = 'High',
+                   PositionalBinding = $true,
+                   SupportsPaging = $false,
+                   SupportsShouldProcess = $false)]
+    [OutputType([datetime])]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [long]
+        $ExpirationDate
+    )
+    
+    process
+    {
+        foreach ($date in $ExpirationDate)
+        {
+            switch ($ExpirationDate)
+            {
+                0
+                {
+                    Write-Verbose -Message 'Account has no expiration date'
+                }
+                9223372036854775807
+                {
+                    Write-Verbose -Message 'Account has no expiration date'
+                }
+                default
+                {
+                    [datetime]::FromFileTime($ExpirationDate)
+                }
+            }
+        }
+    }
+}
